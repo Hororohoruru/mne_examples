db2f875d2137eba5e9402082823f70bb54a3ea96
Juan Jesus Torre <juanjesustorre@gmail.com>
2020-08-07 16:39:49 +0200

    changed some things to adjust to EEG

--- assignment/assignment.py
+++ assignment/assignment.py
@@ -13,6 +13,7 @@ TODO:
 """
 
 import argparse
+import glob
 import os
 
 import mne
@@ -53,43 +54,34 @@ save = args.save
 
 
 # Some global arguments
-root_dir, save_dir, cal_fname, ctc_fname = get_data_paths()
+root_dir = '/home/jtorretr/neuroimaging/eeg_data/neurosynchrone/data'
+save_dir = '/home/jtorretr/neuroimaging/eeg_data/neurosynchrone/data/derivatives'
 
-if file_type == 'passive':
-    event_dict = {'Auditory 300Hz': 6,  # See trigger_codes.txt
-                  'Auditory 600Hz': 7,
-                  'Auditory 1200Hz': 8,
-                  'Visual Checkerboard': 9}
+if not os.path.exists(save_dir):
+    os.mkdir(save_dir)
 
 # First, we get the subject IDs and load their data
-sub_ids = get_subject_ids(root_dir=root_dir)
-raw_names = [os.path.join(root_dir, f"{sub_id}/{file_type}/{file_type}_raw.fif")
-             for sub_id in sub_ids]
+raw_names = glob.glob(os.path.join(root_dir, '*.set'))
 
 # The keys of this dictionary are the subject IDs, and the values are preloaded raw files
-raw_dict = {sub_id: mne.io.read_raw_fif(file, preload=True)
-            for sub_id, file in zip(sub_ids, raw_names)}
+raw_dict = {os.path.basename(file).split('.')[0]: mne.io.read_raw_eeglab(file, preload=True) for
+            file in raw_names}
 
-# The resulting list consists of tuples with (sub_id, sss_raw) information
-sss_raw_list = Parallel(n_jobs=len(sub_ids))(delayed(run_maxfilter)(
-                        sub_id=sub_id,
-                        raw=raw,
-                        cal_fname=cal_fname,
-                        ctc_fname=ctc_fname,
-                        results_dir=save_dir) for sub_id, raw in raw_dict.items())
+# Apply notch filter and band-pass filter to the data
+# The resulting list consists of tuples with (sub_id, filter_raw) information
+filter_raw_list = Parallel(n_jobs=len(raw_names))(delayed(filter_data)(
+                           sub_id=sub_id,
+                           raw=raw,
+                           results_dir=save_dir) for sub_id, raw in raw_dict.items())
 
 print("")
-print('Maxfilter passed on all subjects')
+print(f'Notch filter at {power_line} and band pass filter between {l_freq} and {h_freq} '
+      f'applied to all subjects')
 print("")
-
-if save:
-    save_files(sss_raw_list, save_dir, prefix='maxwell', suffix=file_type)
-    print("Saved files with maxwell filter applied")
-    print("")
 
 # Subject by subject, enter interactive mode to visually inspect the data and mark bad channels,
 # that will be excluded after you manually close the interactive console pressing Ctrl+D
-for id, data in sss_raw_list:
+for id, data in filter_raw_list:
     print(f"Interactive mode for sub {id}. Please check the data and mark bad channels "
           f"if necessary (adding them to data.info['bads'])")
     print("")
@@ -99,36 +91,24 @@ for id, data in sss_raw_list:
 
     shell()
 
-    data.pick_types(meg=True, eog=True, stim=True, ecg=True, exclude='bads')
-
-# Apply notch filter and band-pass filter to the data
-# The resulting list consists of tuples with (sub_id, filter_raw) information
-filter_raw_list = Parallel(n_jobs=len(sub_ids))(delayed(filter_data)(
-                           sub_id=sub_id,
-                           raw=raw,
-                           results_dir=save_dir) for sub_id, raw in sss_raw_list)
-
-print("")
-print(f'Notch filter at {power_line} and band pass filter between {l_freq} and {h_freq} '
-      f'applied to all subjects')
-print("")
+    data.pick_types(eeg=True, eog=True, ecg=True, stim=True, exclude='bads')
 
 if save:
     save_files(filter_raw_list, save_dir, prefix='filtered', suffix=file_type)
     print("Saved notch and band pass filters applied")
     print("")
 
-# Now apply ICA correction for heartbeats and blinks
-ica_raw_list = Parallel(n_jobs=len(sub_ids))(delayed(run_ica_correction)(
-                        sub_id=sub_id,
-                        raw=raw,
-                        results_dir=save_dir) for sub_id, raw in filter_raw_list)
-
-print("")
-print(f'ICA run for all subjects')
-print("")
-
-if save:
-    save_files(sss_raw_list, save_dir, prefix='ica_corrected', suffix=file_type)
-    print("Saved files with ICA correction applied for EOG and ECG artifacts")
-    print("")
+# # Now apply ICA correction for heartbeats and blinks
+# ica_raw_list = Parallel(n_jobs=len(raw_names))(delayed(run_ica_correction)(
+#                         sub_id=sub_id,
+#                         raw=raw,
+#                         results_dir=save_dir) for sub_id, raw in filter_raw_list)
+#
+# print("")
+# print(f'ICA run for all subjects')
+# print("")
+#
+# if save:
+#     save_files(ica_raw_list, save_dir, prefix='ica_corrected', suffix=file_type)
+#     print("Saved files with ICA correction applied for EOG and ECG artifacts")
+#     print("")
